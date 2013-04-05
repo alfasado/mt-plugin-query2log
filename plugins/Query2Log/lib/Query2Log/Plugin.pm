5afe465dbe76e82d81d904e41539db18c4bd12fe
Alfasado Inc <webmaster@alfasado.jp>
2013-04-05 17:18:53 +0900

    Added. at AdminScript(Query2LogDebugMode and Query2LogAtError).

--- plugins/Query2Log/lib/Query2Log/Plugin.pm
+++ plugins/Query2Log/lib/Query2Log/Plugin.pm
@@ -1,9 +1,11 @@
 package Query2Log::Plugin;
 use strict;
 
+use MT::Log;
+
 sub _post_run {
     my $app = MT->instance;
-    if ( (ref $app) !~ /^MT::App::/ ) {
+    if ( ( ref $app ) !~ /^MT::App::/ ) {
         return;
     }
     if ( my $level = MT->config( 'Query2LogDebugMode' ) ) {
@@ -13,9 +15,42 @@ sub _post_run {
         }
         $app->log( {
                 message => MT->instance->query_string,
-                level => 4,
+                catgory => 'query2log',
+                level => MT::Log::DEBUG(),
         } );
     }
+}
+
+sub _error {
+    my ( $cb, $app, $param, $tmpl ) = @_;
+    if ( MT->config( 'Query2LogAtError' ) ) {
+        $app->log( {
+                message => $param->{ error } .
+                ' : ' . MT->instance->query_string,
+                catgory => 'query2log',
+                level => MT::Log::ERROR(),
+        } );
+    }
+}
+
+sub _error_log {
+    my ( $cb, $obj ) = @_;
+    my $app = MT->instance;
+    if ( ( ref $app ) !~ /^MT::App::/ ) {
+        return 1;
+    }
+    return 1 if $obj->level != MT::Log::ERROR();
+    return 1 if $obj->category eq 'query2log';
+    if ( MT->config( 'Query2LogAtError' ) ) {
+        
+        $app->log( {
+                message => $obj->message .
+                ' : ' . $app->query_string,
+                catgory => 'query2log',
+                level => MT::Log::ERROR(),
+        } );
+    }
+    return 1;
 }
 
 sub _hdlr_query2log {
@@ -25,7 +60,7 @@ sub _hdlr_query2log {
         $message .= ' : ';
     }
     my $app = MT->instance;
-    if ( (ref $app) !~ /^MT::App::/ ) {
+    if ( ( ref $app ) !~ /^MT::App::/ ) {
         return '';
     }
     my $q = $app->query_string;
@@ -39,7 +74,8 @@ sub _hdlr_query2log {
     $message .= $q;
     $app->log( {
             message => $message,
-            level => 4,
+            catgory => 'query2log',
+            level => MT::Log::DEBUG(),
     } );
     if ( $args->{ print } ) {
         return $message;
